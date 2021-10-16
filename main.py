3f52938010a412811bce00cb00ea6178103d7920
Viorel Moldovan <viorelmoldovan@gmail.com>
2021-10-16 12:43:55 +0300

    version 2

--- main.py
+++ main.py
@@ -1,81 +1,116 @@
 from flask import Flask, request, jsonify
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
-# Init app
+
+# Init app, ma
 app = Flask(__name__)
+#db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-# Init database
-def db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect("top_movies.db")
-    except sqlite3.Error as e:
-        print(e)
-    return conn
+
+# Set path to db
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'top_movies.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+
+
+# Create movie model
+class Movie(db.Model):
+    __tablename__ = "movies"
+    rank = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __init__(self, rank, title, year, rating):
+        self.rank = rank
+        self.title = title
+        self.year = year
+        self.rating = rating
+
+    def __repr__(self):
+        return f"Movie {self.title} from {self.year}, ranked: {self.rank} and rated: {self.rating}"
+
+
+# Create Schema
+class MovieSchema(ma.Schema):
+    class Meta:
+        fields = ("rank", "title", "year", "rating")
+
+
+# Init Schema
+movie_schema = MovieSchema()#strict=True
+movies_schema = MovieSchema(many=True,) #strict=True
+
 
 @app.route("/")
 def home():
     return "Go to /movies"
 
+
 # C R U D
-@app.route("/movies", methods=["GET", "POST"])
-def movies():
-    conn = db_connection()
-    c = conn.cursor()
+# Get all the movies
+@app.route("/movies", methods=['GET'])
+def get_movies():
+    movies = Movie.query.all()
+    result = movies_schema.dump(movies)
+    return jsonify(result)
 
-    if request.method == "GET":
-        all = c.execute("""SELECT * FROM movies""")
-        movies = [dict(rank=row[0], title=row[1], year=row[2], rating=row[3])
-                  for row in all.fetchall()]
-        if movies is not None:
-            return jsonify(movies)
+# Get one movie
+@app.route("/movies/<rank>", methods=['GET'])
+def get_movie(rank):
+    movie = Movie.query.get(rank)
+    return movie_schema.jsonify(movie)
 
-    if request.method == "POST":
-        new_rank = request.form["rank"]
-        new_title = request.form["title"]
-        new_year = request.form["year"]
-        new_rating = request.form["rating"]
-        sql = """INSERT INTO movies (rank, title, year, rating) VALUES (?, ?, ?, ?)"""
-        cursor = c.execute(sql, (new_rank, new_title, new_year, new_rating))
-        conn.commit()
-        return f"Movie with id {cursor.lastrowid} was created."
+# Create a movie entry
+@app.route("/movies", methods=['POST'])
+def add_movie():
+    rank = request.json['rank']
+    title = request.json['title']
+    year = request.json['year']
+    rating = request.json['rating']
+    print(rank, title, year, rating)
 
+    new_movie = Movie(rank, title, year, rating)
 
-@app.route("/movies/<int:rank>", methods=["GET", "PUT", "DELETE"])
-def movie(rank):
-    conn = db_connection()
-    c = conn.cursor()
-    movie = None
-    if request.method == "GET":
-        c.execute("""SELECT * FROM movies WHERE rank=?""", (rank,))
-        rows = c.fetchall()
-        for row in rows:
-            movie = row
-        if movie is not None:
-            return jsonify(movie), 200
-        else:
-            return "Error, not found", 404
+    db.session.add(new_movie)
+    db.session.commit()
 
-    if request.method == "PUT":
-        sql = """UPDATE movies SET title=?, year=?, rating=? WHERE rank=?"""
-        title = request.form["title"]
-        year = request.form["year"]
-        rating = request.form["rating"]
-        updated_movie = {
-            "rank": rank,
-            "title": title,
-            "year": year,
-            "rating": rating
-        }
-        conn.execute(sql, (title, year, rating, rank))
-        conn.commit()
-        return jsonify(updated_movie)
+    return movie_schema.jsonify(new_movie)
 
-    if request.method == "DELETE":
-        sql = """DELETE FROM movies WHERE rank=?"""
-        conn.execute(sql, (rank,))
-        conn.commit()
-        return f"Movie with id {rank} was deleted.", 200
+# Update a movie entry
+@app.route("/movies/<rank>", methods=['PUT'])
+def update_movie(rank):
+    movie = Movie.query.get(rank)
+
+    rank = request.json['rank']
+    title = request.json['title']
+    year = request.json['year']
+    rating = request.json['rating']
+
+    movie.rank = rank
+    movie.title = title
+    movie.year = year
+    movie.rating = rating
+
+    db.session.commit()
+
+    return movie_schema.jsonify(movie)
+
+# Delete a movie entry
+@app.route("/movies/<rank>", methods=['DELETE'])
+def delete_movie(rank):
+    movie = Movie.query.get(rank)
+    if movie is None:
+        return {"Movie": "was not found"}
+    db.session.delete(movie)
+    db.session.commit()
+    return {"Movie": "was deleted"}
+
 
 # Run server
 if __name__ == "__main__":
